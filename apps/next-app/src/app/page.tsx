6d71589c2ec2bcf55b293b96dd3035422f917580
Neil <Neil10241126@gmail.com>
2025-03-23 17:04:23 +0800

    01

--- apps/next-app/src/app/page.tsx
+++ apps/next-app/src/app/page.tsx
@@ -15,7 +15,7 @@ const Home = () => {
         />
         <ol>
           <li>
-            Get started by editing <code>src/app/page.tsx100fafaff</code>.
+            Get started by editing <code>src/app/page.tsxff100fafaff</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
